cb7297b4ce77dd60f0f8a7c504c1404202797c04
allythiel <allythiel@gmail.com>
2021-06-23 18:53:38 -0500

    Add Middleware

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const repoContext = require('./repository/repository-wrapper');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded( { extend: true }));
+
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
