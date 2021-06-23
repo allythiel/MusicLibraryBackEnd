8205a3eb59772c3cd777e7696227f5a282d24f3c
allythiel <allythiel@gmail.com>
2021-06-23 18:01:16 -0500

    Add Get Request for Find All Songs

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const repoContext = require('./repository/repository-wrapper');
+
 const app = express();
 
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
+
+app.get('/', (req, res) => {
+    return res.send('Postman Test');
+});
+
+app.get('/api/products', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
