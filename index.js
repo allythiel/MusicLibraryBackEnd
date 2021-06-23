0f6684bab3eca7620435c2145d8386722277d7a3
allythiel <allythiel@gmail.com>
2021-06-23 18:37:15 -0500

    Update to API Songs vs API Products

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.get('/', (req, res) => {
     return res.send('Postman Test');
 });
 
-app.get('/api/products', (req, res) => {
+app.get('/api/songs', (req, res) => {
     const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
 });
 
-app.get('/api/products/:id', (req, res) => {
+app.get('/api/songs/:id', (req, res) => {
     const id = req.params.id;
     const song = repoContext.songs.findSongById(id);
     return res.send(song);
